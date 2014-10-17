1d24fddffb4f0c7a0841dad239a48d3504cca68b
Lia Coleman <i347c475@mit.edu>
2014-10-16 21:16:48 -0400

    add constructor/getPos/setPos for Ball.java

--- src/warmup/Ball.java
+++ src/warmup/Ball.java
@@ -1,5 +1,21 @@
 package warmup;
 
-public class Ball {
+import physics.*;
 
+public class Ball {
+    Vect velocity;
+    Geometry.DoublePair position;
+    
+    public Ball(Vect velocity, Geometry.DoublePair position){
+        this.velocity = velocity;
+        this.position = position;
+    }
+    
+    public Geometry.DoublePair getPosition(){
+        return this.position;
+    }
+    
+    public void setPosition(Geometry.DoublePair newPosition){
+        this.position = newPosition;
+    }
 }
--- src/warmup/Main.java
+++ src/warmup/Main.java
@@ -2,14 +2,52 @@ package warmup;
 
 /**
  * TODO: put documentation for your class here
+ * board
+ *   -has 4 linesegments
+ *   -has a ball
+ *   
+ *   -toString(): array of arrays 
+ *   
+ *   -run(ballstart,ballend)
+ *      while True:
+ *      
+     *      if timeToPrint(currentTime):
+     *          print new string
+     *          
+     *      lastTime = currentTime()
+     *      currentTime = System.currentTimeinMillis()
+     *      
+     *      recalculate ball's new x and y
+     *          translate(startx, starty, velocity, timedelta) --> 
+         *      if ball's new x-coord/y-coord out of board:
+         *       Vect reflectWall(LineSegment line, Vect velocity)
+         *      else:
+         *       set ball's x and y
+ *   
+ *  
+ * ball 
+ * _pos (x,y) --> geometry.doublepair()
+ *  
+ *  Vect velocity = length of vector is speed, dir is velocity dir
+ *  toString() --> *
+ *  getPositionOnBoard ()
+ *  
+ *  setLocation() 
+ *  getLocation() --> geometry.doubleapir
+ *  
+ * print 10-20 times per second
+ * 
  */
+
 public class Main {
     
     /**
      * TODO: describe your main function's command line arguments here
      */
+    
     public static void main(String[] args) {
-        
+        //initialize() = create board
+        //print board 10-20 times
     }
     
 }
