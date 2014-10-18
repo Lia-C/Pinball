c5bd06cdc2b782a8f820974a637023f97fcdfca5
Jordan Ugalde <Jordan@Laptop.(none)>
2014-10-18 17:48:46 -0400

    minor changes to absorber

---
+++ src/absorber.java
@@ -0,0 +1,20 @@
+
+public class absorber {
+
+    private final int height;
+    private final int width;
+    /**
+     * 
+     * @param height must be <=20
+     * @param width must be <=20
+     */
+    public absorber(int height, int width){
+        this.height=height;
+        this.width=width;
+    }
+    
+    public String toString(){
+        return "=";
+    }
+    
+}
