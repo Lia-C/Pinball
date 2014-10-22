14158cf17ec4c6d465815f8e52d9d3b5a51ea61b
Roberto <robsoto@mit.edu>
2014-10-22 01:30:42 -0400

    working on Flipper action

--- src/LeftFlipper.java
+++ src/LeftFlipper.java
@@ -11,9 +11,11 @@ import physics.*;
  * Rotates 90 degrees when triggered.
  */
 public class LeftFlipper implements Gadget {
-    private final int xCor, yCor, orientation; //orientation must be 0 or 90
-    private final LineSegment flipper;
-    private final Circle pivot, endpoint;
+    private final int xCor, yCor, orientation; 
+    private double time;
+    private LineSegment flipper;
+    private Circle endpoint;
+    private final Circle pivot;
     private boolean flipping; 
     private boolean isVertical;
     
@@ -133,6 +135,10 @@ public class LeftFlipper implements Gadget {
         this.time = time;
     }
     
+    public boolean isActing() {
+        return flipping;
+    }
+    
     public boolean isOccupying(int x, int y) {
         if (orientation == 0) {
             if (isVertical) {
@@ -184,20 +190,36 @@ public class LeftFlipper implements Gadget {
      */
     public void Action(Ball ball) { //TODO
         flipping = true;
-        double angleRotated = 0;
+        Angle angleRotated = new Angle(Math.toRadians(ANGULAR_VELOCITY * time));
+        if (angleRotated.compareTo(Angle.DEG_90) >= 0) { 
+            flipping = false;
+            angleRotated = Angle.DEG_90;
+            isVertical = !isVertical;
+        }
+        flipper = Geometry.rotateAround(flipper, pivot.getCenter(), angleRotated);
+        endpoint = Geometry.rotateAround(endpoint, pivot.getCenter(), angleRotated);
         
         LineSegment[] lineSegments = new LineSegment[]{flipper};
         Circle[] circles = new Circle[]{pivot, endpoint};
         
         if (Util.getPartOfGadgetThatBallWillCollideWith(circles, lineSegments, ball) instanceof LineSegment) {
             LineSegment wall = (LineSegment)Util.getPartOfGadgetThatBallWillCollideWith(circles, lineSegments, ball);
-            Geometry.reflectWall(wall, ball.getVelocity(), COEFFICIENT_OF_REFLECTION);
+            if (flipping) {
+                Geometry.reflectRotatingWall(wall, pivot.getCenter(), ANGULAR_VELOCITY, ball.getCircle(), ball.getVelocity(), COEFFICIENT_OF_REFLECTION);
+            } else {
+                Geometry.reflectWall(wall, ball.getVelocity(), COEFFICIENT_OF_REFLECTION);
+            }
         } else if (Util.getPartOfGadgetThatBallWillCollideWith(circles, lineSegments, ball) instanceof Circle) { 
             Circle corner = (Circle)Util.getPartOfGadgetThatBallWillCollideWith(circles, lineSegments, ball);
-            Geometry.reflectCircle(corner.getCenter(), ball.getCircle().getCenter(), ball.getVelocity(), COEFFICIENT_OF_REFLECTION);
+            if (flipping && corner.getCenter().x() == pivot.getCenter().x() && corner.getCenter().y() == pivot.getCenter().y()) {
+                //corner is pivot, is not moving
+                Geometry.reflectCircle(corner.getCenter(), ball.getCircle().getCenter(), ball.getVelocity(), COEFFICIENT_OF_REFLECTION);
+            } else if(flipping) { //corner is endpoint, is moving
+                Geometry.reflectRotatingCircle(endpoint, pivot.getCenter(), ANGULAR_VELOCITY, ball.getCircle(), ball.getVelocity(), COEFFICIENT_OF_REFLECTION);
+            } else { //not flipping
+                Geometry.reflectCircle(corner.getCenter(), ball.getCircle().getCenter(), ball.getVelocity(), COEFFICIENT_OF_REFLECTION);
+            }
         }
-        flipping = false;
-        isVertical = !isVertical;
     }
     
     public boolean isEmpty() {
