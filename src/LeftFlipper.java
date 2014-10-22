a9d95dc0a779e124143959716b19dd6098935b74
Roberto <robsoto@mit.edu>
2014-10-22 02:03:05 -0400

    Flipper done, but untested. TriangleBumper updated for changes in Gadget interface

--- src/LeftFlipper.java
+++ src/LeftFlipper.java
@@ -16,11 +16,12 @@ public class LeftFlipper implements Gadget {
     private LineSegment flipper;
     private Circle endpoint;
     private final Circle pivot;
+    private final Gadget[] toTrigger;
     private boolean flipping; 
     private boolean isVertical;
     
     private final double COEFFICIENT_OF_REFLECTION = 0.95;
-    private final double ANGULAR_VELOCITY = 1080.0;
+    private final double ANGULAR_VELOCITY = 1080.0; //LeftFlippers always rotate counterclockwise
     
     //Rep invariant:
     //  xCor, yCor are in the range [0, 18] inclusive
@@ -43,10 +44,11 @@ public class LeftFlipper implements Gadget {
      *          rotation in degrees from the default orientation
      *          must be 0, 90, 180, or 270
      */
-    public LeftFlipper(int xCor, int yCor, int orientation) {
+    public LeftFlipper(int xCor, int yCor, int orientation, Gadget[] toTrigger) {
         this.xCor = xCor;
         this.yCor = yCor;
         this.orientation = orientation;
+        this.toTrigger = toTrigger;
         if (orientation == 0) {
             this.flipper = new LineSegment(xCor, yCor, xCor, yCor+1);
             this.pivot = new Circle(xCor, yCor, 0);
@@ -82,10 +84,11 @@ public class LeftFlipper implements Gadget {
      *          y-coordinate of the upper-left corner of the bumper's bounding box
      *          must be in the range [0,19]
      */
-    public LeftFlipper(int xCor, int yCor) {
+    public LeftFlipper(int xCor, int yCor, Gadget[] toTrigger) {
         this.xCor = xCor;
         this.yCor = yCor;
         this.orientation = 0;
+        this.toTrigger = toTrigger;
         this.flipper = new LineSegment(xCor, yCor, xCor, yCor+1);
         this.pivot = new Circle(xCor, yCor, 0);
         this.endpoint = new Circle(xCor, yCor+1, 0);
@@ -181,14 +184,7 @@ public class LeftFlipper implements Gadget {
         return Util.getMinCollisionTime(circles, lineSegments, ball);
     }
     
-    /**
-     * Mutates the ball's velocity when the ball hits the bumper and 
-     * rotates itself 90 degrees around its pivot point.
-     * 
-     * @param ball
-     *          the ball which hit the bumper
-     */
-    public void Action(Ball ball) { //TODO
+    public void Action() { 
         flipping = true;
         Angle angleRotated = new Angle(Math.toRadians(ANGULAR_VELOCITY * time));
         if (angleRotated.compareTo(Angle.DEG_90) >= 0) { 
@@ -198,7 +194,10 @@ public class LeftFlipper implements Gadget {
         }
         flipper = Geometry.rotateAround(flipper, pivot.getCenter(), angleRotated);
         endpoint = Geometry.rotateAround(endpoint, pivot.getCenter(), angleRotated);
-        
+        time = 0;
+    }
+    
+    public void interactWithBall(Ball ball) {
         LineSegment[] lineSegments = new LineSegment[]{flipper};
         Circle[] circles = new Circle[]{pivot, endpoint};
         
@@ -220,6 +219,10 @@ public class LeftFlipper implements Gadget {
                 Geometry.reflectCircle(corner.getCenter(), ball.getCircle().getCenter(), ball.getVelocity(), COEFFICIENT_OF_REFLECTION);
             }
         }
+    }
+    
+    public Gadget[] trigger() {
+        return toTrigger;
     }
     
     public boolean isEmpty() {
